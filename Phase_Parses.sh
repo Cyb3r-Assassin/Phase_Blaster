375e50127729b6f83a53d8c479a9eacdbcac5de2
Chris Scott <cyb3r-assassin@cyberpunk.me>
2015-03-24 16:57:41 -0500

    Additions

--- Phase_Parses.sh
+++ Phase_Parses.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-for i in nmap/all_hosts/*; do [ -f $i ] && host=$(grep [0-9]/ $i | grep open); [[ -n $host ]] && echo -e "$i\n$host\n"; done
+for i in nmap/all_hosts/*
+  do
+    [ -f $i ] && host=$(grep [0-9]/ $i | grep open)
+    [[ -n $host ]] && echo -e "$i\n$host\n"
+done
